6877a6880615db415cb49e38c708f80f7674d1ce
Neil Petrick <vengantmjolnir@gmail.com>
2022-08-30 17:22:07 -0700

    Adding start of bot commands, and help formatter

---
+++ NebulousConquestHelper/src/bot/BotCommands.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NebulousConquestHelper.src.bot
+{
+    public class BotCommands : BaseCommandModule
+    {
+        [Command("list_fleets")]
+        [Description("List all fleets")]
+        [Aliases("lf")]
+        public async Task ListFleets(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":printer:");
+
+            await ctx.RespondAsync($"{emoji} Listing all the fleets now:");
+        }
+    }
+}
