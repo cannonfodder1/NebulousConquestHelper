17e6f92c66c395c3655f9496293dadc14dcb39a1
Neil Petrick <vengantmjolnir@gmail.com>
2022-09-01 10:24:06 -0700

    Changed namespaces to be consistent

--- NebulousConquestHelper/src/bot/BotCommands.cs
+++ NebulousConquestHelper/src/bot/BotCommands.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NebulousConquestHelper.src.bot
+namespace NebulousConquestHelper
 {
     public class BotCommands : BaseCommandModule
     {
--- NebulousConquestHelper/src/bot/BotHelpFormatter.cs
+++ NebulousConquestHelper/src/bot/BotHelpFormatter.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace NebulousConquestHelper.src.bot
+namespace NebulousConquestHelper
 {
     public class BotHelpFormatter : BaseHelpFormatter
     {
